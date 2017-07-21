07f350d530c3754b47561bd80f502a2f78e853f2
Rafa Huelin <it@engisa.com>
2017-07-21 09:04:53 +0100

    Add scripts and images tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,15 @@ gulp.task('styles', function () {
   console.log('starting styles task');
 });
 
-// Script
+// Scripts
+gulp.task('scripts', function () {
+  console.log('starting script task');
+});
 
 // Images
+gulp.task('images', function () {
+  console.log('starting images task');
+});
 
 gulp.task('default', function () {
   console.log('Starting default task');
