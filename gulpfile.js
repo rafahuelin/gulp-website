6c5208732c73193dfc3f92a4b810d1040f668be0
Rafa Huelin <it@engisa.com>
2017-07-21 13:53:27 +0100

    Add error handling functionality

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 
 // File paths
 var DIST_PATH = 'public/dist';
@@ -21,6 +22,11 @@ gulp.task('html', function() {
 gulp.task('styles', function () {
   console.log('starting styles task');
   return gulp.src(['public/css/reset.css', CSS_PATH])
+    .pipe(plumber(function (err) {
+      console.log('Styles Task Error');
+      console.log(err);
+      this.emit('end');
+    }))
     .pipe(autoprefixer())
     .pipe(concat('styles.css'))
     .pipe(minifyCss())
